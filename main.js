690695a3137f1eb02cfabbb97f5fb94a790a7a17
谢 <921035044@qq.com>
2017-05-12 22:27:05 +0800

    添加注释

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 //引入Vue
 import Vue from 'vue';
 
+//引入路由
 import VueRouter from 'vue-router';
+
+//使用路由
 Vue.use(VueRouter);
+
+//引入mui.css样式
 require('./mainArea/mui/css/mui.css');
 
-
+//引入vue模块
 var index = require('./component/index.vue');
 
 var router = new VueRouter({
@@ -18,5 +23,6 @@ new Vue({
 	el:'#taobao',
 	
 	template:`<div><router-view></router-view></div>`,
+	//需要注入
 	router
 })
